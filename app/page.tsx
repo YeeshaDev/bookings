d3b5dac102c2b30a5f5ff63e27471e2b49f64ca1
YeeashaDev <aishaagunbiade05@gmail.com>
2023-04-29 12:03:27 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+     <h1>lets start here </h1> 
+     <p>This is an airbnb clone app </p>
+     <h2>This is frustrating</h2>
+    </main>
+  )
+}
